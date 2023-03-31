0f891689362064fd027701e27eeb2e1a8e903f97
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-03-31 06:56:17 -0700

    Fix zero arg segfault in 2020/burton
    I did not fix the strange output when more than one arg is specified nor
    do I think I should.
    
    I did not update Dave's comment that it will segfault on zero args nor
    do I think I should (instead I updated the To run section like I've done
    with the many others I've fixed).

--- 2020/burton/prog.c
+++ 2020/burton/prog.c
@@ -1,1 +1,1 @@
-int main(int b,char**i){long long n=B,a=I^n,r=(a/b&a)>>4,y=atoi(*++i),_=(((a^n/b)*(y>>T)|y>>S)&r)|(a^r);printf("%.8s\n",(char*)&_);}
+int main(int b,char**i){if(1[i]){long long n=B,a=I^n,r=(a/b&a)>>4,y=atoi(*++i),_=(((a^n/b)*(y>>T)|y>>S)&r)|(a^r);printf("%.8s\n",(char*)&_);}}
