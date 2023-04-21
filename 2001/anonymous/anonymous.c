e9a3f77ea3b209e63ac3f9c06bb84ad86e5ea706
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-04-21 08:48:16 -0700

    Fix formatting of 2001/anonymous
    Now the macros are restored and the formatting (of variables included
    which spells out a message) is also restored and works fine.

--- 2001/anonymous/anonymous.c
+++ 2001/anonymous/anonymous.c
@@ -1,3 +1,5 @@
+#define processor x86
+
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -6,44 +8,33 @@
 
 #define l int*
 #define F char
- 
-struct stat t;
+ struct stat t;
 #define c return
 #define I (P+=4,*L(P-4,0))
 #define G (signed F)E(*L(P++,0))
-#define C(O,D) E (D[B+V(010)/4+O*10])
+#define C(O,D)E (D[B+V(010)/4+O*10])
 #define U R[4]=E(V(17)-4),*(l)V(021)=
-char M [99],Q[99],b[15000],*ss,*d=b, *z;
+F       M [99],Q[99],b[9999],*ss,*d=b,*z;
 #define O =(n=*(l)V(021),R[4]=E(V(17)+4),n)
 #define p(a,b,c) system((sprintf(a,b,k[1]),c)),z
-#define g (y/010&7)
-#define R (B+13)
-#define x86 (F*)index(ss+V(i ),0100)
-
-#define D(y,n,a,m,i,c) d+=sprintf( d,y,n,a,m,i,c ),(F* ) P
-
-      int *B, i,n,a,r,y ,
-      P ;
-
-
-int* Li(d,r){ return E (3[B+V(010)/4+r*10])&&E (3[B+V(010)/4+r*10])<=d?(int*)(((int)B)+d+E (4[B+V(010)/4+r*10])-E (3[B+V(010)/4+r*10])):(((int)B)^d)>>24&&--r? Li(d,r):(int*)d; } int* L(d, r){ return r=V(014 )&63,Li(d,r); }
-        int* Runi(d,a){ return (ss=strcmp((char*)B+E (4[B+V(010)/4+(V(12)>>16)*10])+E (0[B+V(010)/4+--r*10]),".strtab")?ss:(char*)B+E (4[B+V(010)/4+r*10]),E (1[B+V(010)/4+r*10])-2)? Runi(d,r):0; } int* Run(d, a){ return r=V(014 )&63,Runi(d,a); }
-                         Sca(){ i=((a-=16)+E (4[B+V(010)/4+r*10]))/4,(V(-~i)-P+2?Sca() :0) ; } Scan(){ a=E (5[B+V(010)/4+r*10]),Sca(); } ru(){ i=((a-=16)+E (4[B+V(010)/4+r*10]))/4,(strcmp(ss+V(i),"main")?ru() :0) ; } run(){ a=E (5[B+V(010)/4+r*10]),ru(); }
-E(DX)
-{
-    return *z?
-	DX : DX <<030 |
-	(DX&65280)<<010 |
-	DX>>8 & 65280 |
-	DX>>24&0377;
-}
-
-int* K(k){
-return L(E(k [(int*)V(17)]),0); }
-
-V(v){
-    return E (B[v]);
-}
+#define                         g (y/010&7)
+#define                         R (B+13)
+#define           x86           (F*)index\
+(ss+V(i                         ),0100)
+#define      D(y,n,a,m,i,c      )d+=sprintf( d,y,n,a,m,i,c ),(F*\
+      )                         P
+      l       B,i,n,a,r,y       ,
+      P                         ;
+#define     Tr(an,sl,at,or)     l an##i(d,sl){ c at? an##i(d,r):or; } \
+l an(d,                         sl){ c \
+r=V(014                         )&63,an##i(d,sl); }
+#define add(Ev,Gv) Ev(){ i=((a-=16)+C(r,4))/4,(\
+Gv?Ev()    :0)   ; } Ev##n(){ a=C(r,5),Ev(); }
+Tr(L,r, C(r,3)&&C(r,3)<=d?(l)(((int)B)+d+C(r,4)-C(r,3)):(((int)B)^d)>>24&&--\
+r,(l)d) Tr(Run,a,(ss=strcmp((F*)B+C((V(12)>>16),4)+C(--r,0),A(.strtab))?ss:(F\
+*)B+C(r ,4),C(r,1)-2),0) add(Sca ,V(-~i)-P+2) add(ru,strcmp(ss+V(i),A(main)))
+E(DX) { c *z? DX:DX <<030|(DX&65280)<<010|DX>>8&65280|DX>>24&0377; } l K(k){
+c L(E(k [(l)V(17)]),0); } V(v){ c E(B[v]); }
 
 int
 pain(char *ck, char **k)
