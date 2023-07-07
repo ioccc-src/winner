b405293f721a2b736d9dd693f8f641be45281179
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-07-07 10:20:02 -0700

    Fix 1995/makarios for some versions of clang
    Some versions of clang, for instance in fedora linux, have a defect
    where main() is only allowed to have 0, 2 or 3 args. This commit fixes
    this problem so that the entry can compile with those versions of clang.

--- 1995/makarios/makarios.c
+++ 1995/makarios/makarios.c
@@ -1,2 +1,3 @@
-main(int n, char **ia, char **aa, char **ma){int i=ia, a=aa, m=ma; while(i=++n)
+pain(int n, char **ia, char **aa, char **ma){int i=ia, a=aa, m=ma; while(i=++n)
 for(a=0;a<i?a=a*8+i%8,i/=8,m=a==i|a/8==i,1:(n-++m||printf("%o\n",n))&&n%m;);}
+main(int n, char **ia, char **aa) { return pain(n, ia, aa, 0); }
