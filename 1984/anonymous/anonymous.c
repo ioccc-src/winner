2a917a97279896df49b81338a47d4bba84425ac9
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-07-08 09:44:50 -0700

    Minor fixes in 1984/anonymous
    The anonymous.c no longer triggers a warning that can't be disabled
    (added 'int' before 'i' which is how it was in the original anyway).
    
    Minor fix in the README.md.

--- 1984/anonymous/anonymous.c
+++ 1984/anonymous/anonymous.c
@@ -1,2 +1,2 @@
-i;main(){for(;i["]<i;++i){--i;}"];read('-'-'-',i+++"hello, world!\\
+int i;main(){for(;i["]<i;++i){--i;}"];read('-'-'-',i+++"hello, world!\\
 n",'/'/'/'));}read(j,i,p)void*i;{write(j/p+p,i---j,(int)i/(int)i);}
