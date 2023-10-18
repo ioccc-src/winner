10a947b34e7c3bd0107b2f6795f3e25a878d6394
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-10-18 01:32:07 -0700

    Minor improvements to 1991/fine/{demo.sh,Makefile}
    The demo.sh script now shows a bit more what is going on, at least in so
    far as the text can go either way: instead of showing:
    
        Green terra: Terra green
        Vex: Irk
        [...]
        The rug gary lenT: Gur eht tnel yraG
    
    it shows:
    
        Green terra <->  Terra green
        Vex <->  Irk
        [...]
        The rug gary lenT <->  Gur eht tnel yraG
    
    The Makefile now silences as many warnings as possible.

--- 1991/fine/demo.sh
+++ 1991/fine/demo.sh
@@ -2,22 +2,22 @@
 
 make all || exit 1
 
-echo -n "Green terra: "
+echo -n "Green terra <->  "
 echo "Green terra" |./fine
 
-echo -n "Vex: "
+echo -n "Vex <->  "
 echo "Vex" | ./fine
 
-echo -n "Tang: "
+echo -n "Tang <->  "
 echo "Tang" | ./fine
 
-echo -n "Vend onyx: "
+echo -n "Vend onyx <->  "
 echo "Vend onyx" | ./fine
-echo -n "Cheryl be flashy: "
+echo -n "Cheryl be flashy <->  "
 echo "Cheryl be flashy" | ./fine
-echo -n "Rail: "
+echo -n "Rail <->  "
 echo "Rail" | ./fine
-echo -n "Clerk: "
+echo -n "Clerk <->  "
 echo "Clerk" | ./fine
-echo -n "The rug gary lenT: "
+echo -n "The rug gary lenT <->  "
 echo "The rug gary lenT" | ./fine
