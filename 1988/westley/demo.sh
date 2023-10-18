0fcc400d0a3df96d5fbee2e4afe91942ce7bd4bc
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-10-18 14:10:40 -0700

    Improve 1988/westley/demo.sh
    Check for executable 'westley' before compiling and only _after_
    compilation (if necessary) is the source shown. This way if it's
    already done (as likely the case) the result of the program will be
    shown closer to the source code which is entirely the point. But even if
    it's not already compiled since it's compiled first it'll have the
    result closer to the source (if it fails though it'll not show the
    source at all).

--- 1988/westley/demo.sh
+++ 1988/westley/demo.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 clear
+
+if [[ ! -x "westley" ]]; then
+    echo "$ cc westley.c -o westley"
+    cc -std=gnu89 -Wno-error -Wno-implicit-function-declaration -Wno-return-type    -O3 westley.c -o westley || exit 1
+fi
+
 echo "$ cat westley.c"
 cat westley.c
-
 sleep 1
-
-echo "$ cc westley.c -o westley"
-cc -std=gnu89 -Wno-error -Wno-implicit-function-declaration -Wno-return-type    -O3 westley.c -o westley || exit 1
 
 echo "$ ./westley"
 ./westley
