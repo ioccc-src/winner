2b7fc96c99b42c371b16f0fcc8d97cd350e2c80b
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-11-14 04:03:16 -0800

    Improve 1998/dorssel/try.sh
    Get CC, defaulting to cc.

--- 1998/dorssel/try.sh
+++ 1998/dorssel/try.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # 
-# script to show 1998/dorssel
+# try.sh - demonstrate IOCCC winner 1998/dorssel
+#
+
+# make sure CC is set so that when we do make CC="$CC" it isn't empty. Doing it
+# this way allows us to have the user specify a different compiler in an easy
+# way.
+if [[ -z "$CC" ]]; then
+    CC="cc"
+fi
+
+make CC="$CC" all >/dev/null || exit 1
+
+# clear screen after compilation so that only the entry is shown
+clear
 
 echo "$ echo 'Simple as 123' | ./dorssel" 1>&2
 echo 'Simple as 123' | ./dorssel
