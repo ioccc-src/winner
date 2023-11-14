cc8478d0cfc83a8fe447b93f7a9ddb8d5508e444
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-11-14 03:41:01 -0800

    Improve 1995/dodsond1/try.sh
    Get CC var, defaulting to cc.

--- 1995/dodsond1/try.sh
+++ 1995/dodsond1/try.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+# 
+# try.sh - show IOCCC winner 1995/dodsond1
+#
 
-make all >/dev/null || exit 1
+# get CC
+
+# make sure CC is set so that when we do make CC="$CC" it isn't empty. Doing it
+# this way allows us to have the user specify a different compiler in an easy
+# way.
+if [[ -z "$CC" ]]; then
+    CC="cc"
+fi
+
+make CC="$CC" all >/dev/null || exit 1
+
+# clear screen after compilation so that only the entry is shown
+clear
 
 echo "$ cat try.this.txt"
 cat try.this.txt
