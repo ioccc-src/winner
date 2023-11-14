d0e87f88a61546b33d3d38b7236cecff05f0c858
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-11-14 04:05:03 -0800

    Improve 1998/schnitzi/try.sh
    Get CC, defaulting to cc.

--- 1998/schnitzi/try.sh
+++ 1998/schnitzi/try.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+# 
+# try.sh - demonstrate IOCCC winner 1998/schnitzi
+#
 
-make all || exit 1
+# make sure CC is set so that when we do make CC="$CC" it isn't empty. Doing it
+# this way allows us to have the user specify a different compiler in an easy
+# way.
+if [[ -z "$CC" ]]; then
+    CC="cc"
+fi
+
+make CC="$CC" all >/dev/null || exit 1
+
+# clear screen after compilation so that only the entry is shown
+clear
 
 echo "$ ./schnitzi 3 > sort.c" 1>&2
 ./schnitzi 3 > sort.c
