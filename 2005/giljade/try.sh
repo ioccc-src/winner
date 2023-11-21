43897789a1d46251a9d27f06c90f47913b019d84
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-11-21 04:34:08 -0800

    Fix and improve 2005/giljade/try.sh
    After one exited less(1) the text about leaving the file there for one
    to look at it with an editor was on the same line as other output.
    
    Also use less -X so that the screen is not cleared. That way one can see
    the last part of the file that they looked at.

--- 2005/giljade/try.sh
+++ 2005/giljade/try.sh
@@ -25,10 +25,11 @@ echo 1>&2
 echo "$ ./giljade > out" 1>&2
 ./giljade > out
 echo "For this next step, make sure to keep pressing space (or hold it down) (q = quit)." 1>&2
-echo "$ less out" 1>&2
+echo "$ less -X out" 1>&2
 read -r -n 1 -p "Press any key to continue: "
-less out
+less -X out
 
+echo 1>&2
 echo "We've left the file 'out' for you in case you wish to open it in vi(m). If" 1>&2
 echo "you do we recommend you try holding ctrl-f down in a similar way to how you held" 1>&2
 echo "down space above. Of course you can open it in another editor if you wish." 1>&2
