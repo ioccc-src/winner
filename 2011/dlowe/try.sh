f50ed4e622f4684efa2cd57129146db66cfeaf81
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-11-26 11:10:24 -0800

    Format/typo check and other fixes in 2011/dlowe
    The README.md file has been typo/format checked/fixed.
    
    The try.sh script has been updated with some improvements and additional
    commands.
    
    The bug status was changed to INABIAF as these limitations were
    documented by the author.
    
    I believe this should complete the review of 2011/dlowe.

--- 2011/dlowe/try.sh
+++ 2011/dlowe/try.sh
@@ -17,16 +17,36 @@ make CC="$CC" all >/dev/null
 # clear screen after compilation so that only the entry is shown
 clear
 
+read -r -n 1 -p "Press any key to run: ./dlowe dlowe.c < dlowe-aux-data/ioccc-winlose-trained.net: "
 ./dlowe dlowe.c < dlowe-aux-data/ioccc-winlose-trained.net
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: ./dlowe dlowe < dlowe-aux-data/ioccc-winlose-trained.net: "
 ./dlowe dlowe < dlowe-aux-data/ioccc-winlose-trained.net
+echo 1>&2
 
+read -r -n 1 -p "Press any key to run: ./dlowe xor-0/00 < dlowe-aux-data/xor-trained.net: "
 ./dlowe xor-0/00 < dlowe-aux-data/xor-trained.net
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: ./dlowe xor-0/11 < dlowe-aux-data/xor-trained.net: "
 ./dlowe xor-0/11 < dlowe-aux-data/xor-trained.net
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: ./dlowe xor-1/01 < dlowe-aux-data/xor-trained.net: "
 ./dlowe xor-1/01 < dlowe-aux-data/xor-trained.net
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: ./dlowe xor-1/10 < dlowe-aux-data/xor-trained.net: "
 ./dlowe xor-1/10 < dlowe-aux-data/xor-trained.net
+echo 1>&2
 
+read -r -n 1 -p "Press any key to run: ./dlowe README.html < dlowe-aux-data/english-trained.net: "
 ./dlowe README.html < dlowe-aux-data/english-trained.net
+echo 1>&2
 
+read -r -n 1 -p "Press any key to run: ./dlowe dlowe-aux-data.tar.bz2 < dlowe-aux-data/png-trained.net: "
 ./dlowe dlowe-aux-data.tar.bz2 < dlowe-aux-data/png-trained.net
 
-
+read -r -n 1 -p "Press any key to run:  ./dlowe dlowe.c < dlowe-aux-data/ioccc-winlose-trained.net: "
+./dlowe dlowe.c < dlowe-aux-data/ioccc-winlose-trained.net
