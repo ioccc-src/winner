30847d6aadc79669735018de42b3729ac7047a00
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-12-13 06:41:52 -0800

    Add helper scripts to 2018/endoh2 + formatting
    The run.sh script was fixed to work. It did not work because it had a
    command that does not even exist ('tempfile') and even after that was
    resolved there was another problem to be resolved.
    
    The Makefile rule make python runs the python.sh script but it sets a
    trap so one can exit that script but not exit everything (see below).
    The python3.sh script is what python.sh is but for the make rule python3
    and it works in the same way. The try.sh script runs all of these
    scripts as well as the commands suggested by the judges.
    
    The README.md file has been format/typo checked. For the link (twice
    referenced) that has flashing lights (and quite a few at that) I put a
    warning in parentheses for those who are (like me) sensitive to it (more
    important for some people with epilepsy, depending on how sensitive they
    are, maybe, but I hate flashing text and lights and so do many other
    people).

---
+++ 2018/endoh2/python3.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# python3.sh - make python3 rule script of 2018/endoh2 so one can use this in
+# other scripts, allowing one to interrupt it but continue to the next script
+#
+
+# make sure CC is set so that when we do make CC="$CC" it isn't empty. Doing it
+# this way allows us to have the user specify a different compiler in an easy
+# way.
+if [[ -z "$CC" ]]; then
+    CC="cc"
+fi
+
+make CC="$CC" all >/dev/null || exit 1
+
+CFLAGS="-Wno-strict-prototypes"
+LDFLAGS=
+
+# set up trap so one can go on to next script after interrupting this one in
+# case this script is being run from another
+EXIT=0
+trap 'EXIT=1' INT
+
+rm -f prog_next.c prog_next
+${CC} "${CFLAGS}" prog.c -o prog "${LDFLAGS}"
+while [[ "$EXIT" != 1 ]]; do
+    ./prog | tee prog_next.c;
+    cc prog_next.c -o ./prog "$CFLAGS" "$LDFLAGS"
+done
+exit 0
