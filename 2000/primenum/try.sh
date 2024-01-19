ac860471da98b0c94dd4c1386a82d335b5f2738b
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-01-19 09:12:37 -0800

    Improve 2000/primenum/try.sh

--- 2000/primenum/try.sh
+++ 2000/primenum/try.sh
@@ -16,19 +16,29 @@ make CC="$CC" all >/dev/null || exit 1
 clear
 
 
-echo "$ echo IOCCC 23209 | ./primenum 23209" 1>&2
+read -r -n 1 -p "Press any key to run: echo IOCCC 23209 | ./primenum 23209: "
+echo 1>&2
 echo IOCCC 23209 | ./primenum 23209
 echo 1>&2
 
-echo "$ echo THIS MESSAGE IS IN ALL CAPS | ./primenum 32" 1>&2
+read -r -n 1 -p "Press any key to run: echo THIS MESSAGE IS IN ALL CAPS | ./primenum 32: "
+echo 1>&2
 echo THIS MESSAGE IS IN ALL CAPS | ./primenum 32
 echo 1>&2
 
 rm -f try.sh.txt
-echo "$ ./primenum 101 < try.sh | ./primenum n > try.sh.txt" 1>&2
+read -r -n 1 -p "Press any key to run: ./primenum 101 < try.sh | ./primenum n > try.sh.txt: "
+echo 1>&2
 ./primenum 101 < try.sh | ./primenum n > try.sh.txt
+echo 1>&2
+
+read -r -n 1 -p "Press any key to show try.sh.txt (space = next page, q = quit): "
+echo 1>&2
+less -rEXF try.sh.txt
+echo 1>&2
+
 echo "$ diff try.sh try.sh.txt"
 read -r -n 1 -p "Press any key to see diff (space = next page, q = quit): " 1>&2
-diff try.sh try.sh.txt|less -EXF
+diff try.sh try.sh.txt | less -rEXF
 
 rm -f try.sh.txt
