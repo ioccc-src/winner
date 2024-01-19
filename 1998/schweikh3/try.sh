0b27c467f2367c7ff28de1bcba6d1e8b29b9d619
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-01-19 08:31:51 -0800

    Improve 1998/schweikh3/try.sh

--- 1998/schweikh3/try.sh
+++ 1998/schweikh3/try.sh
@@ -15,16 +15,30 @@ make CC="$CC" all >/dev/null || exit 1
 # clear screen after compilation so that only the entry is shown
 clear
 
-echo "$ cp -f schweikh3 file1"
-cp -f schweikh3 file1
-echo "$ cp -f schweikh3 file2"
-cp -f schweikh3 file2
-echo "$ cp -f schweikh3.c file3"
-cp -f schweikh3.c file3
-echo "$ ln -f file3 file4"
-ln -f file3 file4
-echo "$ find . -type f -name 'file?' | ./samefile"
-find . -type f -name 'file?' | ./samefile
+read -r -n 1 -p "Press any key to run: cp -vf schweikh3 file1: "
+echo 1>&2
+cp -vf schweikh3 file1
+echo 1>&2
 
-echo "find . -type f -print | ./samefile" 1>&2
+read -r -n 1 -p "Press any key to run: cp -vf schweikh3 file2: "
+echo 1>&2
+cp -vf schweikh3 file2
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: cp -vf schweikh3.c file3: "
+echo 1>&2
+cp -vf schweikh3.c file3
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: ln -vf file3 file4: "
+echo 1>&2
+ln -vf file3 file4
+read -r -n 1 -p "Press any key to run: find . -type f -name 'file?' | ./samefile: "
+echo 1>&2
+find . -type f -name 'file?' | ./samefile
+echo 1>&2
+
+read -r -n 1 -p "Press any key to run: find . -type f -print | ./samefile: "
+echo 1>&2
 find . -type f -print | ./samefile
+echo 1>&2
