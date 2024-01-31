5259fc9b6944fd316bc92e94ab36e89f94f92b5e
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-01-31 06:25:28 -0800

    Improve wording in 2018/endoh2/try.sh

--- 2018/endoh2/try.sh
+++ 2018/endoh2/try.sh
@@ -15,12 +15,12 @@ make CC="$CC" all >/dev/null || exit 1
 # clear screen after compilation so that only the entry is shown
 clear
 
-read -r -n 1 -p "Press any key to continue (ctrl-c/intr = skip to next early, runs 20 seconds): "
+read -r -n 1 -p "Press any key to continue (ctrl-c/intr = skip to next step early, runs 20 seconds): "
 echo 1>&2
 ./run.sh
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run a few times (ctrl-c/intr exits script): "
+read -r -n 1 -p "Press any key to run a few times: "
 echo 1>&2
 ./prog | tee prog_next.c
 make prog_next
@@ -28,12 +28,12 @@ make prog_next
 make prog_next
 ./prog_next | tee prog_next.c
 
-read -r -n 1 -p "Press any key to sleep 1 sec between runs (intr/ctrl-c to exit): "
+read -r -n 1 -p "Press any key to sleep 1 sec between runs (intr/ctrl-c to stop): "
 echo 1>&2
 ./python.sh
 echo 1>&2
 
-read -r -n 1 -p "Press any key run without sleeping (intr/ctrl-c to exit): "
+read -r -n 1 -p "Press any key run without sleeping (intr/ctrl-c to stop): "
 echo 1>&2
 ./python3.sh
 
