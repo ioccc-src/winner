c3d8609064a3008ae30a0104ea8c62c67498dbd0
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-01-31 09:56:03 -0800

    Fix 2019/mills/try.sh (missing less(1) calls)

--- 2019/mills/try.sh
+++ 2019/mills/try.sh
@@ -18,17 +18,16 @@ echo 1>&2
 echo "This may take a while:"
 make test-64bit
 
-
 echo "$ less IOCCC-Rules-Guidelines.output.txt" 1>&2
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-less -rEXF IOCCC-Rules-Guidelines.output.txt
+less -rEXFK IOCCC-Rules-Guidelines.output.txt
 echo 1>&2
 
 echo "$ less IOCCC-hints.output.txt" 1>&2
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-less -rEXF IOCCC-hints.output.txt
+less -rEXFK IOCCC-hints.output.txt
 echo 1>&2
 
 echo "$ less Eugene_Onegin.output.txt" 1>&2
@@ -36,17 +35,17 @@ echo "You should be able to safely ignore the warnings about this file" 1>&2
 echo "being a binary file." 1>&2
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-less -rEXF Eugene_Onegin.output.txt
+less -rEXFK Eugene_Onegin.output.txt
 echo 1>&2
 
 echo "$ ./prog < IOCCC-Rules-Guidelines.cp98_0.175 |head -n 100"
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-./prog < IOCCC-Rules-Guidelines.cp98_0.175 |head -n 100
+./prog < IOCCC-Rules-Guidelines.cp98_0.175 |head -n 100 | less -rEXFK
 echo 1>&2
 
 echo "$ ./prog < Shakespeare.cp04_1.633 | head -n 100" 1>&2
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-./prog < Shakespeare.cp04_1.633 | head -n 100
+./prog < Shakespeare.cp04_1.633 | head -n 100 | less -rEXFK
 echo 1>&2
