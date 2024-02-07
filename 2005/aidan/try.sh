ba758e897dfa18a56901d4105e77c9a31886e83f
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-02-07 03:47:11 -0800

    Minor update to less options in 2005/aidan/try

--- 2005/aidan/try.sh
+++ 2005/aidan/try.sh
@@ -20,12 +20,12 @@ AIDAN="aidan"
 echo "$ ./$AIDAN < blank.sudoku" 1>&2
 read -r -n 1 -p "Press any key to run: ./$AIDAN < blank.sudoku (space = next page, q = quit): "
 echo 1>&2
-./"$AIDAN" < blank.sudoku | less -rEXF
+./"$AIDAN" < blank.sudoku | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./$AIDAN U < blank.sudoku (space = next page, q = quit): "
 echo 1>&2
-./"$AIDAN" U < blank.sudoku | less -rEXF
+./"$AIDAN" U < blank.sudoku | less -rEXFK
 echo 1>&2
 echo "What was the difference between the previous command?" 1>&2
 echo "More specifically, what did it do?" 1>&2
@@ -33,7 +33,7 @@ echo 1>&2
 
 read -r -n 1 -p "Press any key to run: echo IOCCC | ./$AIDAN (space = next page, q = quit): "
 echo 1>&2
-echo IOCCC | ./"$AIDAN" | less -rEXF
+echo IOCCC | ./"$AIDAN" | less -rEXFK
 echo 1>&2
 echo "What just happened?" 1>&2
 echo 1>&2
@@ -41,18 +41,18 @@ echo 1>&2
 
 read -r -n 1 -p "Press any key to run: echo IOCCC | ./$AIDAN 42 (space = next page, q = quit): "
 echo 1>&2
-echo IOCCC | ./"$AIDAN" 42 | less -rEXF
+echo IOCCC | ./"$AIDAN" 42 | less -rEXFK
 echo 1>&2
 echo "Seeing that, was the 'echo IOCCC' relevant?" 1>&2
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./$AIDAN < insane1.sudoku (space = next page, q = quit): "
-./"$AIDAN" < insane1.sudoku | less -rEXF
+./"$AIDAN" < insane1.sudoku | less -rEXFK
 
 read -r -n 1 -p "Do you with to run the test suite (y/n)? "
 if [[ "$REPLY" = "y" || "$REPLY" = "Y" ]]; then
     echo 1>&2
     read -r -n 1 -p "Press any key to run: make test (space = next page, q = quit): "
     echo 1>&2
-    make test | less -rEXF
+    make test | less -rEXFK
 fi
