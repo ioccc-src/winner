890f2bede12259e56491934904e4d552e378da48
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-02-07 08:54:50 -0800

    Minor improvements to 2006/birken/try.sh

--- 2006/birken/try.sh
+++ 2006/birken/try.sh
@@ -15,67 +15,67 @@ make CC="$CC" all >/dev/null || exit 1
 # clear screen after compilation so that only the entry is shown
 clear
 
-read -r -n 1 -p "Press any key to run: ./birken < adder.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < adder.tofu: "
 echo 1>&2
 ./birken < adder.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < buffer.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < buffer.tofu: "
 echo 1>&2
 ./birken < buffer.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < computer.tofu 2>/dev/null (space = next page, q = quit): "
+read -r -n 1 -p "Press any key to run: ./birken < computer.tofu (space = next page, q = quit): "
 echo 1>&2
-./birken < computer.tofu 2>/dev/null | less -rEXF
+./birken < computer.tofu 2>/dev/null | less -rEXFK
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < counter.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < counter.tofu: "
 echo 1>&2
 ./birken < counter.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < divider.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < divider.tofu: "
 echo 1>&2
 ./birken < divider.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < lfsr.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < lfsr.tofu: "
 echo 1>&2
 ./birken < lfsr.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < memory.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < memory.tofu: "
 echo 1>&2
 ./birken < memory.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < multiplier.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < multiplier.tofu: "
 echo 1>&2
 ./birken < multiplier.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < multiplier2.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < multiplier2.tofu: "
 echo 1>&2
 ./birken < multiplier2.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < oneline.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < oneline.tofu: "
 echo 1>&2
 ./birken < oneline.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < shift.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < shift.tofu: "
 echo 1>&2
 ./birken < shift.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < zuse.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < zuse.tofu: "
 echo 1>&2
 ./birken < zuse.tofu 2>/dev/null
 echo 1>&2
 
-read -r -n 1 -p "Press any key to run: ./birken < zuse2.tofu 2>/dev/null: "
+read -r -n 1 -p "Press any key to run: ./birken < zuse2.tofu: "
 echo 1>&2
 ./birken < zuse2.tofu 2>/dev/null
 echo 1>&2
