50fcdc497d034d73384d82f3e6df5bfe18f19d1f
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-02-17 08:06:04 -0800

    Minor updates to 2011/hamaji/try.sh

--- 2011/hamaji/try.sh
+++ 2011/hamaji/try.sh
@@ -17,7 +17,7 @@ clear
 
 read -r -n 1 -p "Press any key to run: ./hamaji < dragon.nono (q = quit, space = next page): "
 echo 1>&2
-./hamaji < dragon.nono | less -rEXF
+./hamaji < dragon.nono | less -rEXFK
 echo 1>&2
 
 rm -f dragon dragon2
@@ -27,55 +27,55 @@ if [[ -n "$PERL" && -f "$PERL" && -x "$PERL" ]]; then
     echo 1>&2
     echo "$ $PERL c2nono.pl hamaji.c | ./hamaji | tee dragon"
     read -r -n 1 -p "Press any key to continue (space =  next page, q = quit): "
-    "$PERL" c2nono.pl hamaji.c | ./hamaji | tee dragon | less -rEXF
+    "$PERL" c2nono.pl hamaji.c | ./hamaji | tee dragon | less -rEXFK
     echo 1>&2
     echo "$ ./hamaji < dragon.nono | tee dragon2" 1>&2
     read -r -n 1 -p "Press any key to run: ./hamaji < dragon.nono | tee dragon2 (space = next page, q = quit): "
     echo 1>&2
-    ./hamaji < dragon.nono | tee dragon2 | less -rEXF
+    ./hamaji < dragon.nono | tee dragon2 | less -rEXFK
     read -r -n 1 -p "Press any key to run: diff -s dragon dragon2: "
     echo 1>&2
     diff -s dragon dragon2
 else
-    echo "Perl not found, will not run other steps." 1>&2
+    echo "Perl not found, will not run perl related steps." 1>&2
 fi
 rm -f dragon dragon2
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < random.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < random.nono | less -rEXF
+./hamaji < random.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < soccer.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < soccer.nono | less -rEXF
+./hamaji < soccer.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < codegolf.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < codegolf.nono | less -rEXF
+./hamaji < codegolf.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < face.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < face.nono | less -rEXF
+./hamaji < face.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < penguin.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < penguin.nono | less -rEXF
+./hamaji < penguin.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < samurai.nono (q = quit, space = next page): "
 echo 1>&2
-./hamaji < samurai.nono | less -rEXF
+./hamaji < samurai.nono | less -rEXFK
 echo 1>&2
 
 echo "$ ./hamaji < conway-game-of-life.nono" 1>&2
 read -r -n 1 -p "Press any key to continue (space = next page, q = quit): "
 echo 1>&2
-./hamaji < conway-game-of-life.nono | less -rEXF
+./hamaji < conway-game-of-life.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < no-solution.nono (no solution): "
@@ -90,11 +90,11 @@ echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < plus.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < plus.nono | less -rEXF
+./hamaji < plus.nono | less -rEXFK
 echo 1>&2
 
 read -r -n 1 -p "Press any key to run: ./hamaji < smiley.nono (space = next page, q = quit): "
 echo 1>&2
-./hamaji < smiley.nono | less -rEXF
+./hamaji < smiley.nono | less -rEXFK
 
 exit 0
