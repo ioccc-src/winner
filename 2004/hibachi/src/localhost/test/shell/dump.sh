b9e60828f1b11d780bd55e8da6af0330c170c827
Landon Curt Noll <github-mail@asthe.com>
2024-03-11 00:29:54 -0700

    fix shellcheck errors in 2004/hibachi/src/localhost/test/shell/dump.sh

--- 2004/hibachi/src/localhost/test/shell/dump.sh
+++ 2004/hibachi/src/localhost/test/shell/dump.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cat <<'EOT'
 <table border="1" cellspacing="0" cellpadding="0">
@@ -9,9 +9,10 @@ env | sed -e 's/^\(.[^\=]*\)=\(.*\)$/<tr><td class="normal">\1<\/td><td class="n
 
 echo "</table>"
 
-	if [ "$REQUEST_METHOD" = "POST" -a X"$CONTENT_LENGTH" != X ]; then
-STUFF=`dd bs=1 count=$CONTENT_LENGTH 2>/dev/null`
-COUNT=`printf $STUFF | wc -c`
+	if [[ -z $REQUEST_METHOD ]]; then REQUEST_METHOD=''; fi
+	if [[ $REQUEST_METHOD == "POST" && -n $CONTENT_LENGTH ]]; then
+STUFF=$(dd bs=1 count="$CONTENT_LENGTH" 2>/dev/null)
+COUNT=$(printf "%s" "$STUFF" | wc -c)
 
 cat <<EOT
 <table border="1" cellspacing="0" cellpadding="0">
@@ -21,7 +22,7 @@ cat <<EOT
 </table>
 EOT
 
-if [ $CONTENT_LENGTH -eq $COUNT ]; then
+if [[ $CONTENT_LENGTH -eq $COUNT ]]; then
 	echo "[ OK  ] CONTENT_LENGTH = bytes read"
 else
 	echo "[ BAD ] CONTENT_LENGTH != bytes read"
