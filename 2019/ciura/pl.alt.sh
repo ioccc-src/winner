b7ee3e8686794f40f17adc75834975bcfa18f582
Landon Curt Noll <github-mail@asthe.com>
2024-03-10 22:49:17 -0700

    fix shellcheck errors in 2019/ciura/pl.alt.sh

--- 2019/ciura/pl.alt.sh
+++ 2019/ciura/pl.alt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -x
-LC_ALL=C ./getwords.sh pl | LC_ALL=C grep -vE '^[^aiouwz]$' | LC_ALL= ./prog.alt aąbcćdeęfghijklłmnńoóprsśtuwyzźż
+LC_ALL=C ./getwords.sh pl | LC_ALL=C grep -vE '^[^aiouwz]$' | LC_ALL='' ./prog.alt aąbcćdeęfghijklłmnńoóprsśtuwyzźż
