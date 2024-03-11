cb32af90d54c2cbef4cddeb306e7a1e1420d8c2c
Landon Curt Noll <github-mail@asthe.com>
2024-03-10 23:45:13 -0700

    fix shellcheck error in 2018/burton2/freqcount.sh

--- 2018/burton2/freqcount.sh
+++ 2018/burton2/freqcount.sh
@@ -3,4 +3,4 @@ function iskeyword {
    awk 'BEGIN{f="c11";while(getline<f)k[$1]=0}
 	{if($1 in k)k[$1]++}END{for(i in k)print k[i],i}'
 }
-cat $* | ./prog -t | iskeyword | sort -k1nr
+cat "$@" | ./prog -t | iskeyword | sort -k1nr
