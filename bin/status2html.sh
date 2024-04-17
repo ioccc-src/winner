bcc8a12c1970ae30f533a6775170cc67f5100cff
Landon Curt Noll <github-mail@asthe.com>
2024-04-16 21:13:57 -0700

    fix HTML errors in status.html
    Try to fix things that the Nu HTML check is reporting.
    
    Improve formatting of the contents of `status.json` in `status.html`.
    
    Changed `bin/status2html.sh` VERSION from to "1.2 2024-04-15"
    to "1.3 2024-04-16".
    
    Run `make gen_status` to perform the above and test under macOS.

--- bin/status2html.sh
+++ bin/status2html.sh
@@ -99,7 +99,7 @@ shopt -s globstar	# enable ** to match all files and zero or more directories an
 
 # set variables referenced in the usage message
 #
-export VERSION="1.2 2024-04-15"
+export VERSION="1.3 2024-04-16"
 NAME=$(basename "$0")
 export NAME
 export V_FLAG=0
@@ -467,10 +467,8 @@ esac
 
     # write status.json content
     #
-    echo '<div id="content">'
     echo '# status.json'
-    echo '</div>'
-    echo '```json'
+    echo '``` <!---json-->'
     cat "$STATUS_JSON"
     echo '```'
 } | if [[ -z $NOOP ]]; then
